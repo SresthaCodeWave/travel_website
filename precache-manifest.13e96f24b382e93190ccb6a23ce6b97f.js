27273b37c43e5783c372b7065495ab93935a9079
Srestha Chattopadhyay <srestha29b@gmail.com>
2022-08-05 22:39:33 +0530

    Updates

---
+++ precache-manifest.13e96f24b382e93190ccb6a23ce6b97f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5adfc31474f24a00e4183b5653d1f03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90b98e43b4e7c674e8a9",
+    "url": "/static/css/main.b71c3146.chunk.css"
+  },
+  {
+    "revision": "ec1be5ba959f95d81f4c",
+    "url": "/static/js/2.2f124b65.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.2f124b65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90b98e43b4e7c674e8a9",
+    "url": "/static/js/main.1cf47c7b.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
